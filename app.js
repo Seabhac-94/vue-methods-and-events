40be3dede0b151939c986a2c98761b91f3eb8f50
Zac Allen <zac.allen@outlook.com>
2021-06-28 09:38:51 +0200

    added methods and events for inputting name

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name:''
     };
   },
   methods: {
+    setName(event, secondName){
+      this.name = event.target.value + ' ' + secondName
+    },
     addCounter(num){
       this.counter = this.counter + num
     },
